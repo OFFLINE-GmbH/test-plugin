bd78b3eca4939bca626a012b1d02bdb473a210f6
Tobias Kündig <tobias@offline.swiss>
2020-10-16 09:57:55 +0200

    Added in-place sorting

--- controllers/Trees.php
+++ controllers/Trees.php
@@ -9,14 +9,18 @@ use Backend\Classes\Controller;
 class Trees extends Controller
 {
     public $implement = [
-        'Backend.Behaviors.ListController'
+        'Backend.Behaviors.ListController',
+        'Backend.Behaviors.ReorderController',
     ];
 
     public $listConfig = [
         'members' => 'config_members_list.yaml',
+        'categories_flat' => 'config_categories_flat_list.yaml',
         'categories' => 'config_categories_list.yaml',
         'channels' => 'config_channels_list.yaml'
     ];
+
+    public $reorderConfig = 'config_reorder.yaml';
 
     public $requiredPermissions = ['october.test.access_plugin'];
 
